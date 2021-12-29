d79a520fb42ca67c7f9063afbf30f11514c03516
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-12-29 22:33:27 +0530

    add confetti

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+//confetti
+const confettiElement = document.getElementById('my-canvas');
+const confettiSettings = { target: confettiElement };
+const confetti = new ConfettiGenerator(confettiSettings);
+
 const daysRemaining=document.querySelector("#daysRemaining")
 const hoursRemaining=document.querySelector("#hoursRemaining")
 const minutesRemaining=document.querySelector("#minutesRemaining")
 const secondsRemaining=document.querySelector("#secondsRemaining")
+const H1=document.querySelector('h1')
 
 let presentDate=new Date();
-let levelOneStart=new Date(2022,00,02)
-
-
+let levelOneStart=new Date(2021,11,30)
+levelOneStart.setHours( levelOneStart.getHours()-1)
+levelOneStart.setMinutes( levelOneStart.getMinutes() -28)
 
 function timer(presentDate){
     let difference=levelOneStart.getTime()-presentDate.getTime()
@@ -21,8 +27,27 @@ function timer(presentDate){
     secondsRemaining.innerHTML=seconds
 }
 
-setInterval(()=>{
+function reset(){
+    confetti.render();
+
+    daysRemaining.innerHTML=0
+    hoursRemaining.innerHTML=0
+    minutesRemaining.innerHTML=0
+    secondsRemaining.innerHTML=0
+
+    clearInterval(trigger)
+    H1.innerHTML="Let The Fun Begin!"
+}
+
+let trigger=setInterval(()=>{
+    //passing present every second and triggering timer fn
     presentDate=new Date()
-    timer(presentDate)
+    if(presentDate>levelOneStart){
+        reset()
+    }else{
+        timer(presentDate)
+    }
 },1000)
 
+
+    
